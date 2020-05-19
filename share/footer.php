6c827e8cb026358cd948a10548eda442b4fb7360
Bowen He <hebowen360@gmail.com>
2020-05-19 15:52:56 +1200

    Login and register system
    business login system

---
+++ share/footer.php
@@ -0,0 +1,17 @@
+<!-- Footer -->
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<!------ Include the above in your HEAD tag ---------->
+
+<footer class="container-fluid text-center bg-lightgray">
+
+        <div class="copyrights" style="margin-top:25px;">
+           <label><i>powered by</i></label> <img src="img/logo_long.jpg" alt=""  width="150px" height= "30px"/>
+                <br>
+                <span>11 Apollo drive,Rosedale<br/>(+64)224112551</span>
+            <p style="color:#888888;font-size:11px;font-weight:700"> &copy 2020 WEBZILLA,All right reserved</p>
+        </div>
+</footer>
---
+++ share/header_main.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="img/logo_long.jpg" alt="" width="150px" height= "30px">
+  </a>
+</nav>
